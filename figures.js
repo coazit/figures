23b5b01e0d00cff1e950a2399e4791a56d294cac
coazit <coazit@gmail.com>
2022-01-29 16:01:57 -0500

    update funciones geometricas with html

--- figures.js
+++ figures.js
@@ -39,3 +39,55 @@ function areaCirculo (radio){
     return (Math.pow(radio, 2)) * PI;
 }
 console.groupEnd();
+
+//Function de HTML CUADRADO
+function calcularPerimetroCuadrado() {
+    const lado = document.getElementById("InputCuadrado");    
+    const value = lado.value;
+    alert(perimetroCuadrado(value));
+}
+
+function calcularAreaCuadrado() {
+    const lado = document.getElementById("InputCuadrado");    
+    const value = lado.value;   
+    alert(areaCuadrado(value));
+}
+
+
+//Function de HTML TRIANGULO
+function calcularPerimetroTriangulo() {
+    const lado1 = document.getElementById("InputLado1");    
+    const value1 = Number(lado1.value);
+    const lado2 = document.getElementById("InputLado2");    
+    const value2 = Number(lado2.value);
+    const base = document.getElementById("InputBase");    
+    const valuebase = Number(base.value);
+
+    alert(perimetroTriangulo(value1, value2, valuebase));
+}
+
+
+function calcularAreaTriangulo() {
+    const lado1 = document.getElementById("InputLado1");    
+    const value1 = lado1.value;
+    const lado2 = document.getElementById("InputLado2");    
+    const value2 = lado2.value;
+    const base = document.getElementById("InputBase");    
+    const valuebase = base.value;
+
+    alert(areaTriangulo(value1, valuebase));
+}
+
+
+//Function de HTML TRIANGULO
+function calcularPerimetroCirculo() {
+    const radio = document.getElementById("InputRadio");
+    const radiovalue = radio.value;
+    alert(perimetroCirculo(radiovalue));
+}
+function calcularAreaCirculo() {
+    const radio = document.getElementById("InputRadio");
+    const radiovalue = radio.value;
+    alert(areaCirculo(radiovalue));
+}
+
